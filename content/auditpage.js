251402e303b82abbdf9ef67187275b05f6ee86c0
theniall <NiallCasey92@gmail.com>
2016-09-16 22:37:42 -0400

    Did Nothing
    - New Icon
    - Updated Name
    - Updated Description
    - Fixed jQuery loading in content_scripts -- load order was incorrect.
    - Removed Old Icon
    - Updated auditpage.js for the index iteration

--- content/auditpage.js
+++ content/auditpage.js
@@ -1,1 +1,11 @@
-console.log("k");
+$("dt").each(function (index) {
+
+  console.log($(this).text().trim());
+
+  // use $(this).text().trim() to get all the keywords
+  // if the keyword matches one of the keywords that we're looking for,
+  // do a console.log() of the matching <dd> (not the <dt>)
+
+});
+
+  // timeSets, optionGroups, prices, sizes
