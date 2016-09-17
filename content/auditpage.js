70c0057907e8cffaa72b038ab216105b6f3f0a16
theniall <NiallCasey92@gmail.com>
2016-09-17 15:50:03 -0400

    Determine dt index number
    Switch/case to determine the index number of the dt element containing a
    keyword.

--- content/auditpage.js
+++ content/auditpage.js
@@ -1,11 +1,20 @@
-$("dt").each(function (index) {
+$("dt").each(function (index) {   // determine the index number of the dt element
+  var searchResult = $(this).text().trim();
+  switch (searchResult) {
+    case "timesets":
+    case "optionGroups":
+    case "prices":
+    case "sizes":
+      return console.log(index); 
+      break;
+  };
+});
 
-  console.log($(this).text().trim());
+
 
   // use $(this).text().trim() to get all the keywords
   // if the keyword matches one of the keywords that we're looking for,
   // do a console.log() of the matching <dd> (not the <dt>)
 
-});
 
-  // timeSets, optionGroups, prices, sizes
+  // timesets, optionGroups, prices, sizes
