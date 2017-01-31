1d2367804053bdaddf124a0a171e25b0b973bc52
Francisco <fmapilis@gmail.com>
2017-01-31 15:56:58 -0500

    separate out mb3 audits from other action types

--- content/auditpage.js
+++ content/auditpage.js
@@ -8,169 +8,189 @@
 // });}
 
 var visualDiffFunction = function() {
-    $("dt").each(function (index) {   // determine the index number of <dt>
-      var searchResult = $(this).text().trim();
-      switch (searchResult) {
-        case "optionGroups": // hash diffing
-        case "prices":
-        case "sizes":
-        case "options":
-        case "restriction":
-        case "timesets":
-          $("dd").eq(index).each(function(index) { // match <dd>'s index with <dt>'s
+    var audits = $("table.table.table-condensed > tbody > tr")
+
+    audits.each( function(auditIndex) {
+      var action = $(this).find("td")[1]
+      var auditType = $(action).find("div.muted")[1].textContent.trim()
+
+      if(auditType == "menu builder 3 update") {
+
+        // do something here for MB3
+
+      }
+      else {
+        var changesDt = $(this).find("td.changes > dl > dt")
+        var changesDd = $(this).find("td.changes > dl > dd")
+
+        changesDt.each( function(dtIndex) {
+          var searchResult = $(this).text().trim();
+          switch (searchResult) {
+            case "optionGroups": // hash diffing
+            case "prices":
+            case "sizes":
+            case "options":
+            case "restriction":
+            case "timesets":
+              debugger
+              $(changesDd).eq(dtIndex).each(function(ddIndex) { // match <dd>'s index with <dt>'s
+                  var beforeData = $(this).attr("data-old");
+                  var afterData = $(this).attr("data-new");
+                  var $rubyHash = $(this).text().trim();
+                  if (beforeData == "") { // make data-old empty json object
+                    beforeData = "{}";
+                  }
+                  if (afterData == "") { // make data-new empty json object
+                    afterData = "{}";
+                  }
+
+
+
+                  var left = hashToJSON(beforeData);
+                  var right = hashToJSON(afterData);
+
+                  if (beforeData != afterData){
+                    // for (var i in left) {
+                    //   if (left[i].hasOwnProperty('name')) {
+                    //     right[i].identifier = left[i].name;
+                    //     for (var j in left[i].options) {
+                    //       if (left[i].options[j].hasOwnProperty('name')) {
+                    //         right[i].options[j].identifier = left[i][j].options.name;
+                    //       }
+                    //     }
+                    //   }
+                    // }
+                    var delta = jsondiffpatch.diff(left, right);
+                    $(this).html(jsondiffpatch.formatters.html.format(delta));
+                  }
+                  else {
+                    var delta = jsondiffpatch.diff(left, right);
+                    $(this).html("No Change");
+                  };
+
+              });
+              break;
+            case "id": //string diffing name excluded
+            case "name":
               var beforeData = $(this).attr("data-old");
               var afterData = $(this).attr("data-new");
-              var $rubyHash = $(this).text().trim();
               if (beforeData == "") { // make data-old empty json object
                 beforeData = "{}";
               }
               if (afterData == "") { // make data-new empty json object
                 afterData = "{}";
               }
-
-              var left = hashToJSON(beforeData);
-              var right = hashToJSON(afterData);
+              var left = JSON.parse(JSON.stringify({val: beforeData}));
+              var right = JSON.parse(JSON.stringify({val: afterData}));
+              var delta = jsondiffpatch.diff(left, right);
 
               if (beforeData != afterData){
-                // for (var i in left) {
-                //   if (left[i].hasOwnProperty('name')) {
-                //     right[i].identifier = left[i].name;
-                //     for (var j in left[i].options) {
-                //       if (left[i].options[j].hasOwnProperty('name')) {
-                //         right[i].options[j].identifier = left[i][j].options.name;
-                //       }
-                //     }
-                //   }
-                // }
-                var delta = jsondiffpatch.diff(left, right);
                 $(this).html(jsondiffpatch.formatters.html.format(delta));
               }
-              else {
+              break;
+            case "available":
+            case "orderType":
+            case "description":
+            case "discountable":
+            case "combinable":
+            case "special":
+            case "couponOnly":
+            case "fulfilledByDeliveryService":
+            case "sizeType":
+            case "override_ready_at":
+            case "ends_at":
+            case "hours_owner_id":
+            case "hours_owner_type":
+            case "hours_text":
+            case "order_type":
+            case "starts_at":
+            case "perOrderLimit":
+            case "perAccountOrderLimit":
+            case "orderForLaterLeadTime":
+            case "visible":
+            case "delivery_days":
+            case "notified_by_phone":
+            case "status":
+            case "sourceRestaurantID":
+            case "registration_status":
+            case "icons":
+            case "restaurant_id":
+            case "theme":
+            case "domain_name":
+            case "palette":
+            case "registration_status":
+            case "logo_file_name":
+            case "logo_content_type":
+            case "logo_file_size":
+            case "logo_fingerprint":
+            case "logo_updated_at":
+            case "yelp_id":
+            case "latitude":
+            case "longitude":
+            case "delivery_service_id":
+            case "order_for_later_min_minutes_from_open":
+            case "order_for_later_max_days_ahead":
+            case "order_for_later_max_orders_per_time_slot":
+            case "processing_type":
+            case "loyalty_cash_accrual_rate":
+            case "tax_group_id":
+            case "salesforce_rep_email":
+            case "time_zone":
+            case "primary_color":
+            case "secondary_color":
+            case "exposure":
+            case "city":
+            case "override_ready_at":
+            case "zip_code":
+            case "state":
+            case "restaurant_category_id":
+            case "orderForLaterLeadTime":
+            case "descriptors":
+            case "address_1":
+            case "created_at":
+            case "updated_at":
+            case "promotional_message":
+            case "override_make_time":
+            case "starts_on":
+            case "ends_on":
+            case "delivery_instructions":
+            case "processing_fee":
+            case "notification_phone":
+            case "notification_email":
+            case "ach_addenda":
+            case "order_for_later_accepted":
+              $(changesDd).eq(dtIndex).each(function(ddIndex) {
+                var beforeData = $(this).attr("data-old");
+                var afterData = $(this).attr("data-new");
+                var marketTimeZone = determineTimeZone();
+                  if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/.test(beforeData)) { // Date reformatting
+                    beforeData = new Date(beforeData).toLocaleString('en-US', { timeZone: marketTimeZone });
+                  }
+                  if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.000Z$/.test(afterData)) { // Date reformatting
+                    afterData = new Date(afterData).toLocaleString('en-US', { timeZone: marketTimeZone });
+                  }
+                if (beforeData == "") { // make data-old empty json object
+                  beforeData = "{}";
+                }
+                if (afterData == "") { // make data-new empty json object
+                  afterData = "{}";
+                }
+                var left = JSON.parse(JSON.stringify({val: beforeData}));
+                var right = JSON.parse(JSON.stringify({val: afterData}));
                 var delta = jsondiffpatch.diff(left, right);
-                $(this).html("No Change");
-              };
 
-          });
-          break;
-        case "id": //string diffing name excluded
-        case "name":
-          var beforeData = $(this).attr("data-old");
-          var afterData = $(this).attr("data-new");
-          if (beforeData == "") { // make data-old empty json object
-            beforeData = "{}";
-          }
-          if (afterData == "") { // make data-new empty json object
-            afterData = "{}";
-          }
-          var left = JSON.parse(JSON.stringify({val: beforeData}));
-          var right = JSON.parse(JSON.stringify({val: afterData}));
-          var delta = jsondiffpatch.diff(left, right);
-
-          if (beforeData != afterData){
-            $(this).html(jsondiffpatch.formatters.html.format(delta));
-          }
-          break;
-        case "available":
-        case "orderType":
-        case "description":
-        case "discountable":
-        case "combinable":
-        case "special":
-        case "couponOnly":
-        case "fulfilledByDeliveryService":
-        case "sizeType":
-        case "override_ready_at":
-        case "ends_at":
-        case "hours_owner_id":
-        case "hours_owner_type":
-        case "hours_text":
-        case "order_type":
-        case "starts_at":
-        case "perOrderLimit":
-        case "perAccountOrderLimit":
-        case "orderForLaterLeadTime":
-        case "visible":
-        case "delivery_days":
-        case "notified_by_phone":
-        case "status":
-        case "sourceRestaurantID":
-        case "registration_status":
-        case "icons":
-        case "restaurant_id":
-        case "theme":
-        case "domain_name":
-        case "palette":
-        case "registration_status":
-        case "logo_file_name":
-        case "logo_content_type":
-        case "logo_file_size":
-        case "logo_fingerprint":
-        case "logo_updated_at":
-        case "yelp_id":
-        case "latitude":
-        case "longitude":
-        case "delivery_service_id":
-        case "order_for_later_min_minutes_from_open":
-        case "order_for_later_max_days_ahead":
-        case "order_for_later_max_orders_per_time_slot":
-        case "processing_type":
-        case "loyalty_cash_accrual_rate":
-        case "tax_group_id":
-        case "salesforce_rep_email":
-        case "time_zone":
-        case "primary_color":
-        case "secondary_color":
-        case "exposure":
-        case "city":
-        case "override_ready_at":
-        case "zip_code":
-        case "state":
-        case "restaurant_category_id":
-        case "orderForLaterLeadTime":
-        case "descriptors":
-        case "address_1":
-        case "created_at":
-        case "updated_at":
-        case "promotional_message":
-        case "override_make_time":
-        case "starts_on":
-        case "ends_on":
-        case "delivery_instructions":
-        case "processing_fee":
-        case "notification_phone":
-        case "notification_email":
-        case "ach_addenda":
-        case "order_for_later_accepted":
-          $("dd").eq(index).each(function(index) {
-            var beforeData = $(this).attr("data-old");
-            var afterData = $(this).attr("data-new");
-            var marketTimeZone = determineTimeZone();
-              if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/.test(beforeData)) { // Date reformatting
-                beforeData = new Date(beforeData).toLocaleString('en-US', { timeZone: marketTimeZone });
-              }
-              if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.000Z$/.test(afterData)) { // Date reformatting
-                afterData = new Date(afterData).toLocaleString('en-US', { timeZone: marketTimeZone });
-              }
-            if (beforeData == "") { // make data-old empty json object
-              beforeData = "{}";
-            }
-            if (afterData == "") { // make data-new empty json object
-              afterData = "{}";
-            }
-            var left = JSON.parse(JSON.stringify({val: beforeData}));
-            var right = JSON.parse(JSON.stringify({val: afterData}));
-            var delta = jsondiffpatch.diff(left, right);
-
-            if (beforeData != afterData){
-              $(this).html(jsondiffpatch.formatters.html.format(delta));
-            }
-            else {
-              $(this).html("No Change");
-            };
-          });
-          break;
-      };
-    });
+                if (beforeData != afterData){
+                  $(this).html(jsondiffpatch.formatters.html.format(delta));
+                }
+                else {
+                  $(this).html("No Change");
+                };
+              });
+              break;
+          };
+        })
+      }
+    })
   };
 
 function hashToJSON(hash) {
